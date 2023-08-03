69463331e5a2c569398e88fed67b953b95857d27
JanZinch <jan.zinchanka@mail.ru>
2023-08-03 14:50:16 +0300

    feat: cubes

---
+++ Assets/Scripts/Controllers/Mover.cs
@@ -0,0 +1,16 @@
+﻿using Extensions;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] private float _forwardSpeed;
+        [SerializeField] private Rigidbody _rigidbody;
+
+        private void FixedUpdate()
+        {
+            _rigidbody.velocity = _rigidbody.velocity.WithZ(_forwardSpeed);
+        }
+    }
+}
