ca3b6e1724d7ed9d995a94ba1b18e547ce047014
JanZinch <jan.zinchanka@mail.ru>
2023-08-04 17:29:41 +0300

    feat: blocks stack

---
+++ Assets/Scripts/Environment/Block.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Environment
+{
+    public class Block : MonoBehaviour
+    {
+        [SerializeField] private Joint _joint;
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private MeshRenderer _meshRenderer;
+        
+        private Rigidbody ConnectedBody
+        {
+            get =>_joint.connectedBody;
+            set => _joint.connectedBody = value;
+        }
+        
+        public void AttachTo(Block other)
+        {
+            other.ConnectedBody = _rigidbody;
+            transform.position = other.GetAttachingPosition();
+            transform.rotation = Quaternion.identity;
+        }
+
+        private Vector3 GetAttachingPosition()
+        {
+            return transform.position + new Vector3(0.0f, _meshRenderer.bounds.size.y, 0.0f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Environment/BlocksStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment
+{
+    public class BlocksStack : MonoBehaviour
+    {
+        private LinkedList<Block> _blocks;
+
+        private void Awake()
+        {
+            _blocks = new LinkedList<Block>(GetComponentsInChildren<Block>());
+
+            int i = 0;
+            
+            foreach (var block in _blocks)
+            {
+                Debug.Log("i: " + (i++) + " name: " + block.gameObject.name);
+            }
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<Block>(out Block otherBlock) && !_blocks.Contains(otherBlock))
+            {
+                Add(otherBlock);
+            }
+        }
+
+        public void Add(Block block)
+        {
+            block.transform.SetParent(transform);
+            block.transform.SetAsFirstSibling();
+            
+            block.AttachTo(_blocks.First.Value);
+            _blocks.AddFirst(block);
+            
+            int i = 0;
+            
+            foreach (var blo in _blocks)
+            {
+                Debug.Log("i: " + (i++) + " name: " + blo.gameObject.name);
+            }
+        }
+        
+        
+
+    }
+}
