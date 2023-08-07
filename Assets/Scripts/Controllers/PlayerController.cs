8384d37974d448c552da930f0ad7a205f6f41289
JanZinch <jan.zinchanka@mail.ru>
2023-08-07 19:38:10 +0300

    push

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using DG.Tweening;
+using DG.Tweening.Plugins.Core.PathCore;
 using Extensions;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Controllers
 {
@@ -12,11 +15,36 @@ namespace Controllers
         [SerializeField] private float _rightMovementConstraint = 2.0f;
         
         [SerializeField] private Transform _controlledBody;
+
+        [SerializeField] private Transform _pathPointsParent;
         
+        
+        
+        private void Start()
+        {
+            Vector3[] waypoints = new Vector3[_pathPointsParent.childCount];
+
+            int i = 0;
+            
+            foreach (Transform point in _pathPointsParent)
+            {
+                waypoints[i] = point.position;
+                i++;
+            }
+
+            transform.DOLocalPath(waypoints, _forwardSpeed, PathType.Linear, PathMode.Full3D, 10, Color.red).SetEase(Ease.Linear).SetSpeedBased().OnComplete(
+                () =>
+                {
+                    Debug.Log("Success!");
+                });
+            
+            
+        }
+
         private void Update()
         {
-            MoveByZAxis();
-            MoveByXAxis();
+            _controlledBody.localPosition = _controlledBody.localPosition.WithX(Mathf.Clamp(_controlledBody.position.x + ScreenInputAxis.Instance.Delta.x * _maxHorizontalSpeed,
+                _leftMovementConstraint, _rightMovementConstraint));
         }
 
         private void MoveByXAxis()
--- Assets/Scripts/Environment/Block.cs
+++ Assets/Scripts/Environment/Block.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using Configs;
-using Controllers;
-using DG.Tweening;
-using Unity.VisualScripting;
+﻿using Controllers;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Serialization;
 
 namespace Environment
 {
@@ -14,46 +9,14 @@ namespace Environment
         [SerializeField] private Joint _joint;
         [SerializeField] private Rigidbody _rigidbody;
         [SerializeField] private MeshRenderer _meshRenderer;
-        [SerializeField] private bool _isMovable;
-        //[SerializeField] private MoverConfig _moverConfig;
         
         [HideInInspector] [SerializeField] private UnityEvent<Block> _onCollidedWithObstacle;
 
         public UnityEvent<Block> OnCollidedWithObstacle => _onCollidedWithObstacle;
-
-        //private Mover _mover = null;
         
-        //public bool IsMovable => _mover != null;
-
         public bool IsCollided { get; private set; }
 
         public Rigidbody Body => _rigidbody;
-
-        /*public void SetMovable(bool movable)
-        {
-            if (IsMovable == movable)
-                return;
-            
-            if (IsMovable)
-            {
-                Destroy(_mover);
-            }
-            else
-            {
-                _mover = gameObject.AddComponent<Mover>().Launch(_moverConfig, _rigidbody);
-            }
-        }*/
-
-        /*[EasyButtons.Button]
-        public void Launch()
-        {
-            SetMovable(true);
-        }
-
-        private void Awake()
-        {
-            SetMovable(_isMovable);
-        }*/
 
         private void OnCollisionEnter(Collision other)
         {
@@ -66,21 +29,9 @@ namespace Environment
 
         public void AttachTo(Block other)
         {
-            
             Destroy(_joint);
-       
             transform.position = other.GetAttachingPosition();
-
-            //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = transform.position;
-
             _joint = AddConfiguredJoint(gameObject, other._rigidbody);
-            //PresetType TYPE = _jointPreset.GetPresetType()
-            /*_jointPreset.ApplyTo(_joint);
-            _joint.connectedBody = other._rigidbody;
-            _joint.connectedAnchor = Vector3.up * 1.5f;*/
-            //other.ConnectedBody = _rigidbody;
-            
-            
         }
 
         public void Detach()
@@ -90,7 +41,6 @@ namespace Environment
 
         public void Lose()
         {
-            //SetMovable(false);
             Destroy(_joint);
             transform.SetParent(null);
         }
@@ -101,12 +51,7 @@ namespace Environment
             
             character.transform.position = GetAttachingPosition();
             character.transform.rotation = Quaternion.identity;
-
-            /*character.Joint = character.AddComponent<ConfigurableJoint>();
-            _jointPreset.ApplyTo(character.Joint);
-            character.Joint.connectedBody = _rigidbody;
-            character.Joint.connectedAnchor = Vector3.up * 1.5f;*/
-
+            
             character.Joint = AddConfiguredJoint(character.gameObject, _rigidbody);
         }
 
