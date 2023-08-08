49a15681383abb99aecbf3af8d8b9426cfedeb17
JanZinch <jan.zinchanka@mail.ru>
2023-08-08 18:14:31 +0300

    feat: blocks on scene

---
+++ Assets/Scripts/Tools/LevelUpdater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Environment;
+
+namespace Tools
+{
+    public class LevelUpdater : MonoBehaviour
+    {
+        [SerializeField] private Block _blockPrefab;
+    
+        [EasyButtons.Button]
+        public void UpdateBlocks()
+        {
+            GameObject[] updatabaleBlocks = GameObject.FindGameObjectsWithTag("UpdatabaleBlock");
+
+            foreach (var block in updatabaleBlocks)
+            {
+                Instantiate(_blockPrefab, block.transform.position, Quaternion.identity);
+                DestroyImmediate(block.gameObject);
+            }
+        }
+    }
+}
